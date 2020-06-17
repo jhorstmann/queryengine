155d847aada8daad5d58deb89958785c15a36532
Jörn Horstmann <git@jhorstmann.net>
2020-06-18 01:22:19 +0200

    Example for IntSupplier

--- src/test/java/net/jhorstmann/queryengine/AsmExamplesTest.kt
+++ src/test/java/net/jhorstmann/queryengine/AsmExamplesTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
 import java.io.FileOutputStream
+import java.util.function.IntSupplier
 import kotlin.test.assertEquals
 
 class AsmExamplesTest {
@@ -17,6 +18,12 @@ class AsmExamplesTest {
 
 
 
+    private fun dumpToTargetClasses(name: String, bytes: ByteArray?) {
+        FileOutputStream("target/classes/$name.class").use {
+            it.write(bytes)
+        }
+    }
+
     @Test
     fun `should generate empty class`() {
         val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES or ClassWriter.COMPUTE_MAXS)
@@ -24,15 +31,7 @@ class AsmExamplesTest {
         val name = "EmptyClass"
         cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, name, null, "java/lang/Object", emptyArray())
 
-        val init = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null)
-
-        init.visitCode()
-        init.visitVarInsn(Opcodes.ALOAD, 0)
-        init.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
-
-        init.visitInsn(Opcodes.RETURN)
-        init.visitMaxs(1, 1)
-        init.visitEnd()
+        generateEmptyConstructor(cw)
 
         val bytes = cw.toByteArray()
 
@@ -46,9 +45,48 @@ class AsmExamplesTest {
         assertEquals(name, instance.javaClass.simpleName)
     }
 
-    private fun dumpToTargetClasses(name: String, bytes: ByteArray?) {
-        FileOutputStream("target/classes/$name.class").use {
-            it.write(bytes)
-        }
+    @Test
+    fun `should generate callable class`() {
+        val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES or ClassWriter.COMPUTE_MAXS)
+
+        val name = "IntAdder"
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, name, null, "java/lang/Object", arrayOf("java/util/function/IntSupplier"))
+
+        generateEmptyConstructor(cw)
+
+        val mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "getAsInt", "()I", null, null)
+
+        mv.visitCode()
+        mv.visitInsn(Opcodes.ICONST_1)
+        mv.visitInsn(Opcodes.ICONST_2)
+        mv.visitInsn(Opcodes.IADD)
+        mv.visitInsn(Opcodes.IRETURN)
+
+        mv.visitMaxs(2, 0)
+        mv.visitEnd()
+
+        val bytes = cw.toByteArray()
+
+        dumpToTargetClasses(name, bytes)
+
+        val intAdder = OpenClassLoader.defineClass(name, bytes)
+
+        @Suppress("UNCHECKED_CAST")
+        val instance = intAdder.getDeclaredConstructor().newInstance() as IntSupplier
+
+        assertEquals(3, instance.asInt)
     }
+
+    private fun generateEmptyConstructor(cw: ClassWriter) {
+        val init = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null)
+
+        init.visitCode()
+        init.visitVarInsn(Opcodes.ALOAD, 0)
+        init.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
+
+        init.visitInsn(Opcodes.RETURN)
+        init.visitMaxs(1, 1)
+        init.visitEnd()
+    }
+
 }
